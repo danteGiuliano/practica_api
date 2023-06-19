e1156ccaca23c36f0cfaa7e9d0020875a0d68d13
Dante Giuliano <dante.giuliano@est.fi.uncoma.edu.ar>
2023-06-19 13:45:50 -0300

    cambios de Grandle a Maven

--- src/main/java/com/example/demo/DanteApplication.java
+++ src/main/java/com/example/demo/DanteApplication.java
@@ -1,13 +1,13 @@
-package Dante.demo;
+package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DanteApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(DanteApplication.class, args);
 	}
 
 }
--- src/test/java/com/example/demo/DanteApplicationTests.java
+++ src/test/java/com/example/demo/DanteApplicationTests.java
@@ -1,10 +1,10 @@
-package Dante.demo;
+package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class DanteApplicationTests {
 
 	@Test
 	void contextLoads() {
